4eb9defec6cefea8f28ce7948077ec1843dde5a2
Jaeyong Jung <jaeyongjung@Jaeyongs-Laptop.local>
2024-07-14 22:02:46 +0900

    tile

--- 2048-frontend/pages/home/index.js
+++ 2048-frontend/pages/home/index.js
@@ -1,14 +1,13 @@
-'use client';
-
 import Link from 'next/link';
+import React from 'react';
 import styles from './home.module.css';
 
 export default function HomePage() {
   return (
     <div className={styles.container}>
-      <h1 className={styles.title}>2048 Game</h1>
-      <Link href="/game" passHref>
-        <a className={styles.playButton}>Play Game</a>
+      <h1 className={styles.title}>2048 </h1>
+      <Link href="/game" className={styles.playButton}>
+        Play 2048!
       </Link>
     </div>
   );
